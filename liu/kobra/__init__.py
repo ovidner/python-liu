42f18e355aa5bae1c09b8ed22f568808438fe9a1
Olle Vidner <olle@vidner.se>
2014-12-03 10:34:58 +0100

    Clarified exception message

--- liu/kobra/__init__.py
+++ liu/kobra/__init__.py
@@ -41,7 +41,7 @@ class KOBRAClient(object):
         if len(kwargs) != 1 or not any(i in self.accepted_params for i in kwargs.keys()):
             # Raises a TypeError if called with other than exactly 1 argument or with other parameter than the ones
             # accepted.
-            raise TypeError('Called with invalid argument(s) or wrong number of arguments.')
+            raise TypeError('Called with invalid argument(s) and/or wrong number of arguments.')
 
         response = requests.post(self.url, kwargs, auth=(self.user, self.key))
 
